e1209b0f68e2ff2553e15b1a6637995739a0d77d
PureLipper <1090527382@qq.com>
2023-01-02 13:37:20 +0800

    1.2

---
+++ Drifting/app/src/main/java/android/bignerdranch/drifting/MainActivity.java
@@ -0,0 +1,13 @@
+package android.bignerdranch.drifting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
