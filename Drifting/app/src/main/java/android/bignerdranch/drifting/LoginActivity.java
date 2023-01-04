81ebe6f5d21661ff7f04649d820b64064f2ac9a2
PureLipper <1090527382@qq.com>
2023-01-04 10:46:34 +0800

    整合后

--- Drifting/app/src/main/java/android/bignerdranch/drifting/LoginActivity.java
+++ Drifting/app/src/main/java/android/bignerdranch/drifting/LoginActivity.java
@@ -1,13 +1,33 @@
 package android.bignerdranch.drifting;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+
 public class LoginActivity extends AppCompatActivity {
+    private EditText account_text = (EditText) findViewById(R.id.login_account);
+    private EditText password_text = (EditText) findViewById(R.id.login_password);
+    private String account;
+    private String password;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.welcome);
+        new CountDownTimer(2000, 1000)
+        {
+            public void onTick(long millisUntilFinished) {}
+            @Override
+            public void onFinish() {
+                setContentView(R.layout.login);
+            }
+
+
+        }.start();
     }
+    
 }
